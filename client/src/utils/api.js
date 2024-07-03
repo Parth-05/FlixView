8edf8a07e6bb7b4dc0fe3c8a03ccda8b9d282080
Parth-05 <parthmarathe5@gmail.com>
2024-07-02 17:38:29 -0700

    bug fix: Extra api call issue fixed

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -11,9 +11,6 @@ const headers = {
 
 export const fetchDataFromApi = async (url, params) => {
     try {
-        console.log("TMDB_BASE_URL: ", TMDB_BASE_URL)
-        console.log("URL: ", url)
-        console.log(TMDB_BASE_URL + url)
         const { data } = await axios.get( TMDB_BASE_URL + url, {
             headers,
             params
