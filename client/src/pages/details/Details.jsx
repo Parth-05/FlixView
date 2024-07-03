b46b37b6a111d5af1163eba95f767ab41c7ed2f4
Parth-05 <parthmarathe5@gmail.com>
2024-07-03 16:30:51 -0700

    Added Movie Reviews Section

--- client/src/pages/details/Details.jsx
+++ client/src/pages/details/Details.jsx
@@ -8,6 +8,7 @@ import Cast from "./cast/Cast";
 import VideosSection from "./videosSection/VideosSection";
 import Similar from "./carousels/Similar";
 import Recommendation from "./carousels/Recommendation";
+import Reviews from "./reviews/Reviews";
 
 const Details = () => {
     const { mediaType, id } = useParams();
@@ -16,9 +17,14 @@ const Details = () => {
         `/${mediaType}/${id}/credits`
     );
 
+    const { data: reviews, loading: reviewsLoading } = useFetch(
+        `/${mediaType}/${id}/reviews?language=en-US&page=1`
+    );
+
     return (
         <div>
             <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+            <Reviews reviews={reviews} loading={reviewsLoading} />
             <Cast data={credits?.cast} loading={creditsLoading} />
             <VideosSection data={data} loading={loading} />
             <Similar mediaType={mediaType} id={id} />
